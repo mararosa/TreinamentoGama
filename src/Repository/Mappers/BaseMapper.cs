5bf00873ea0911d706e485d3c2264dbd8ab2b5c2
Avanade <Avanade@WIN-0UUXTDCMLL.corp.avanade.org>
2020-02-08 11:44:02 -0300

    mapeamento do banco de dados

--- src/Repository/Mappers/BaseMapper.cs
+++ src/Repository/Mappers/BaseMapper.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Dapper.FluentMap.Dommel.Mapping;
+using Gama.RedeSocial.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Repository.Mappers
 {
-    class BaseMapper
+    public class BaseMapper<TEntity> : DommelEntityMap<TEntity> where TEntity : BaseEntity //classe generica. Ex, usermap herda de baseMapper
     {
+
+        public BaseMapper() //Toda vez que for mapear as entidades usara o basemapper
+            {
+
+            Map(entity => entity.Id).ToColumn("ID").IsKey(); //quando for para tabela de dados as colunas serao criadas dessa maneira
+            Map(e => e.DateCreated).ToColumn("DT_CREATED");
+            Map(e => e.DateUpdated).ToColumn("DT_UPDATED");
+            Map(e => e.Active).ToColumn("ST_ACTIVE");
+
+            }
+
     }
 }
