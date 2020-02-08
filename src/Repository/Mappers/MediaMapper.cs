0d71422561833a6cde58f573842447f710425d84
Avanade <edimara.rosa@hotmail.com>
2020-02-08 12:29:34 -0300

    mapeamento da media

---
+++ src/Repository/Mappers/MediaMapper.cs
@@ -0,0 +1,17 @@
+﻿using Gama.RedeSocial.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Mappers
+{
+    public class MediaMapper : BaseMapper<Media>
+    {
+        public MediaMapper()
+        {
+            ToTable("TB_MEDIA");
+            Map(entity => entity.MediaType.Id).ToColumn("Id_MEDIA_TYPE");
+            Map(entity => entity.Path).ToColumn("DS_PATH");
+        }
+    }
+}
