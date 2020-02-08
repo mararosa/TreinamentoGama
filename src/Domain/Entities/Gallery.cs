024efc178629e3b8c8e481ba2d2ea848f089110b
Avanade <Avanade@WIN-0UUXTDCMLL.corp.avanade.org>
2020-02-08 12:09:56 -0300

    mapeamento do gallery

--- src/Domain/Entities/Gallery.cs
+++ src/Domain/Entities/Gallery.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Gama.RedeSocial.Domain.Entities
 {
-   public class Gallery
+   public class Gallery : BaseEntity
     {
         public string Name { get; set; }
         public List<Media> Medias { get; set; }
---
+++ src/Repository/Mappers/GalleryMapper.cs
@@ -0,0 +1,17 @@
+﻿using Gama.RedeSocial.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Mappers
+{
+    public class GalleryMapper : BaseMapper<Gallery>
+    {
+        public GalleryMapper()
+        {
+            ToTable("TB_GALLERY");
+            Map(entity => entity.Author.Id).ToColumn("DS_AUTHOR");
+            Map(entity => entity.Name).ToColumn("NM_GALLERY");
+        }
+    }
+}
