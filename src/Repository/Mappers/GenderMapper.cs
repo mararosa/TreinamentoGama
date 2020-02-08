ebd10eebbffc9f449b8c3f0e6e030254112c1d05
Avanade <Avanade@WIN-0UUXTDCMLL.corp.avanade.org>
2020-02-08 11:52:57 -0300

    mapeamento do gender

--- src/Repository/Mappers/GenderMapper.cs
+++ src/Repository/Mappers/GenderMapper.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Gama.RedeSocial.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Repository.Mappers
 {
-    class GenderMapper
+    public class GenderMapper : BaseMapper<Gender> //usa o base pq ja fizemos uma classe base q serve para odas as outras
     {
+
+        public GenderMapper()
+        {
+            ToTable("TB_GENDER");
+            Map(entity => entity.Description).ToColumn("DS_GENDER");
+        }
+
     }
 }
