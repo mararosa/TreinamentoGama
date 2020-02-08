83838631876318f4304baeb45abd2599b5a9b701
Avanade <Avanade@WIN-0UUXTDCMLL.corp.avanade.org>
2020-02-08 11:58:55 -0300

    mapeamento do user

---
+++ src/Repository/Mappers/UserMapper.cs
@@ -0,0 +1,23 @@
+﻿using Gama.RedeSocial.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Mappers
+{
+   public class UserMapper : BaseMapper<User>
+    {
+        public UserMapper()
+        {
+            ToTable("TB_USER");
+            Map(entity => entity.Name).ToColumn("NM_USER");
+            Map(entity => entity.Email).ToColumn("DS_EMAIL");
+            Map(entity => entity.Password).ToColumn("DS_PASSWORD");
+            Map(entity => entity.Birthday).ToColumn("DS_BIRTHDAY"); 
+            Map(entity => entity.Gender.Id).ToColumn("ID_GENDER"); 
+            Map(entity => entity.Cover.Id).ToColumn("ID_COVER"); 
+            Map(entity => entity.Avatar.Id).ToColumn("ID_AVATAR"); 
+        }
+
+    }
+}
