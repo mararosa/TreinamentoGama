5da09f9e234c933139c748aa9bb6da86413ba272
Avanade <edimara.rosa@hotmail.com>
2020-02-11 15:14:48 -0300

    atualizando interface invite repository

--- src/Domain/Interfaces/Repositories/IInviteRepository.cs
+++ src/Domain/Interfaces/Repositories/IInviteRepository.cs
@@ -7,5 +7,7 @@ namespace Gama.RedeSocial.Domain.Interfaces.Repositories
 {
     public interface IInviteRepository : IRepositoryBase<Invite>
     {
+        IEnumerable<Invite> GetByUserId(Guid userId);
+        IEnumerable<Invite> GetFriends(Guid userId);
     }
 }
