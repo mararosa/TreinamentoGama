491389fd2b76de18b6e5c82b63bd26a4a4219807
Avanade <edimara.rosa@hotmail.com>
2020-02-11 14:59:10 -0300

    validando media

--- src/Domain/Entities/Media.cs
+++ src/Domain/Entities/Media.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 namespace Gama.RedeSocial.Domain.Entities
 {
     public class Media : BaseEntity
@@ -9,14 +10,25 @@ namespace Gama.RedeSocial.Domain.Entities
             MediaType = new MediaType();
         } //construtor para nao dar erro. Pq quando da uma nova midia ela nao tem tipo e da para acessar. O construtor "libera" o acesso.
 
+        public Guid MediaTypeId { get; set; }
         public MediaType MediaType { get; set; } //para saber se eh foto ou video acesssa aqui
+
         public string Path { get; set; }
 
         public override void Validate()
         {
-            if (string.IsNullOrWhiteSpace(Path)) throw new ArgumentNullException("Path nao pode ser vazio");
+            if (MediaTypeId == Guid.Empty) throw new ArgumentNullException("'MidiaTypeId' não foi preenchido");
 
-            MediaType.Validate();
+            if (string.IsNullOrWhiteSpace(Path)) throw new ArgumentNullException("'Path' não foi preenchido");
         }
+
+        #region MetodoTeste
+        //public override void Validate()
+        //{
+        //    if (string.IsNullOrWhiteSpace(Path)) throw new ArgumentNullException("Path nao pode ser vazio");
+
+        //    MediaType.Validate();
+        //} 
+        #endregion
     }
 }
