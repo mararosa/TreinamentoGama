d8b4dc8b9452e5fb50dfccbcbf8796c5aabc7f54
Avanade <edimara.rosa@hotmail.com>
2020-02-11 14:54:55 -0300

    validando like

--- src/Domain/Entities/Like.cs
+++ src/Domain/Entities/Like.cs
@@ -6,14 +6,33 @@ namespace Gama.RedeSocial.Domain.Entities
 {
     public class Like : BaseEntity
     {
+        public Like()
+        {
+            Post = new Post();
+
+            User = new User();
+        }
+
+        public Guid PostId { get; set; }
         public Post Post { get; set; }
+
+        public Guid UserId { get; set; }
         public User User { get; set; }
 
         public override void Validate()
         {
-            Post.Validate();
-            User.Validate();
+            if (PostId == Guid.Empty) throw new ArgumentNullException("'PostId' não foi preenchido");
+
+            if (UserId == Guid.Empty) throw new ArgumentNullException("'UserId' não foi preenchido");
         }
+
+        #region MetodoTeste
+        //public override void Validate()
+        //{
+        //    Post.Validate();
+        //    User.Validate();
+        //} 
+        #endregion
     }
 
 }
