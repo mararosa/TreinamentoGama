bacaa4c4f5d19777410aed3e20472a9aba5e3a7b
Avanade <edimara.rosa@hotmail.com>
2020-02-11 14:19:30 -0300

    atualizando gallery

--- src/Domain/Entities/Gallery.cs
+++ src/Domain/Entities/Gallery.cs
@@ -6,21 +6,41 @@ namespace Gama.RedeSocial.Domain.Entities
 {
     public class Gallery : BaseEntity
     {
+        public Gallery(string name)
+        {
+            Name = name;
+            Author = new User();
+            Medias = new List<Media>();
+        }
+
         public string Name { get; set; }
         public List<Media> Medias { get; set; }
+
+        public Guid AuthorId { get; set; }
         public User Author { get; set; }
 
         public override void Validate()
         {
-            if (string.IsNullOrWhiteSpace(Name)) throw new ArgumentNullException("O nome deve ser preenchido");
+            if (String.IsNullOrEmpty(Name))
+                throw new ArgumentNullException("'Name' não foi preenchido");
 
-            ValidarListas<Media>(Medias);
-            foreach (var media in Medias)
-            {
-                media.Validate();
-            }
-
-            Author.Validate();
+            if (AuthorId == Guid.Empty)
+                throw new ArgumentNullException("'AuthorId' não foi preenchido")
         }
+
+        #region MetodoTeste
+        //public override void Validate()
+        //{
+        //    if (string.IsNullOrWhiteSpace(Name)) throw new ArgumentNullException("O nome deve ser preenchido");
+
+        //    ValidarListas<Media>(Medias);
+        //    foreach (var media in Medias)
+        //    {
+        //        media.Validate();
+        //    }
+
+        //    Author.Validate();
+        //} 
+        #endregion
     }
 }
