d42607471441b21028a9fba2684f50645cffe5a8
Avanade <edimara.rosa@hotmail.com>
2020-02-11 22:06:50 -0300

    add user controller

---
+++ src/Api/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gama.RedeSocial.Domain.Entities;
+using Gama.RedeSocial.Domain.Interfaces.Applications;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserApplication _app; //vai puxar na API a aplicacao
+                                                
+        public UserController(IUserApplication app) //controller precisa injetar o controller
+        {
+            _app = app;
+        }
+
+        public User Get(Guid id)
+        {
+            return _app.Get(id);
+        }
+    }
+}
